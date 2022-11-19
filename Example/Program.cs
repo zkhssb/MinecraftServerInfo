61fcfae4cbbb506431508c38728eb6b5201e8794
MiuxuE <z164907681@gmail.com>
2022-11-19 13:38:07 +0800

    更改示例

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,9 +1,14 @@
 ﻿using MinecraftServerInfo;
 using MinecraftServerInfo.Model;
 
-MinecraftProtocolVersion minecraftProtocolVersion = MinecraftProtocolVersion.FromGameVersion("1.8.9");
-MinecraftServer minecraftServer = new MinecraftServer("mc.hypixel.net", 25565, minecraftProtocolVersion);
+// 可指定一个协议版本 (可以但是没必要 (划掉)
+// MinecraftProtocolVersion minecraftProtocolVersion = MinecraftProtocolVersion.FromGameVersion("1.8.9");
+MinecraftServer minecraftServer = new MinecraftServer("mc.hypixel.net", 25565);
 CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(3000);
 ServerInfo serverInfo = await minecraftServer.PingAsync(cancellationTokenSource.Token);
 
-Console.WriteLine(serverInfo.PlayerInfo.ToString());
+Console.WriteLine("==========================================================");
+Console.WriteLine(serverInfo.Description.GetText());
+Console.WriteLine(serverInfo.PlayerInfo.ToString());    // 此处可自己获取Players数组
+Console.WriteLine($"{serverInfo.Version.VersionName} ProtocolId:{serverInfo.Version.ProtocolId}");
+Console.WriteLine("==========================================================");
